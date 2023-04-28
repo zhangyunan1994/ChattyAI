4f4e2ca2b46b8977cc7cffbf88f3ad736a5fe47b
zhangyunan <zyndev@gmail.com>
2023-04-28 10:57:54 +0800

    完善 openai 刷新

--- chattyai-java/server/src/main/groovy/cike/chatgpt/Application.groovy
+++ chattyai-java/server/src/main/groovy/cike/chatgpt/Application.groovy
@@ -3,9 +3,11 @@ package cike.chatgpt
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @MapperScan("cike.chatgpt")
 @SpringBootApplication
+@EnableScheduling
 class Application {
 
   static void main(String[] args) {
--- chattyai-java/server/src/main/groovy/cike/chatgpt/component/chat/OpenAiServicePool.groovy
+++ chattyai-java/server/src/main/groovy/cike/chatgpt/component/chat/OpenAiServicePool.groovy
@@ -5,10 +5,13 @@ import cike.chatgpt.repository.OpenAIKeyRepository
 import cike.chatgpt.utils.RandomUtil
 import com.theokanning.openai.service.OpenAiService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 import javax.annotation.PostConstruct
 import java.time.Duration
+import java.util.concurrent.atomic.AtomicLong
+import java.util.stream.Collectors
 
 @Component
 class OpenAiServicePool {
@@ -20,6 +23,8 @@ class OpenAiServicePool {
   private OpenAIKeyRepository repository
 
   List<OpenAiService> enabledKeys = new ArrayList<>()
+
+  Map<String, OpenAiService> openAiServiceMap = new HashMap<>()
 
   @PostConstruct
   void init() {
@@ -37,16 +42,26 @@ class OpenAiServicePool {
     }
   }
 
+  @Scheduled(cron = "0 0/5 * * * ?")
   void refresh() {
     def keyConfigs = repository.findEnableKey()
     if (keyConfigs == null || keyConfigs.size() == 0) {
       enabledKeys.clear()
     } else {
       List<OpenAiService> refreshKeys = new ArrayList<>(keyConfigs.size())
+      Set<String> currentKeys = new HashSet<>()
       for (final def config in keyConfigs) {
-        refreshKeys.add(new OpenAiService(config.openaiKey, Duration.ofSeconds(openAIConfig.timeoutSeconds ?: 10)))
+        String openAIKey = config.openaiKey
+        currentKeys.add(openAIKey)
+        OpenAiService openAIService = openAiServiceMap.computeIfAbsent(openAIKey,
+            k -> new OpenAiService(openAIKey, Duration.ofSeconds(openAIConfig.timeoutSeconds ?: 10)))
+        refreshKeys.add(openAIService)
       }
       enabledKeys = refreshKeys
+
+      // 移除 map 中已经不使用的 openai 实例
+      openAiServiceMap.keySet().stream().filter {!currentKeys.contains(it)} collect(Collectors.toList()) forEach { openAiServiceMap.remove(it) }
+
     }
   }
 }
