a33d40ae1a3812199420e4587c0b4844630b680d
zhangyunan <zyndev@gmail.com>
2023-05-12 18:00:07 +0800

    go go go

--- chattyai-java/openai-java/src/test/java/cike/openai/OpenAiServiceTest.java
+++ chattyai-java/openai-java/src/test/java/cike/openai/OpenAiServiceTest.java
@@ -12,13 +12,20 @@ class OpenAiServiceTest {
   @org.junit.jupiter.api.Test
   void dashboardBillingCreditGrants() {
 
+    String baseUrl = "https://api.openai-proxy.com/";
+    baseUrl = "https://api.openai.com";
+    baseUrl = "http://127.0.0.1:19090";
+
     List<OpenAIAccount> openAIAccountList = new ArrayList<>();
+
+    openAIAccountList.add(new OpenAIAccount("zz", "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"));
 
     DecimalFormat df = new DecimalFormat("#.000");
 
     for (OpenAIAccount openAIAccount : openAIAccountList) {
       ChattyAIService chattyAIService = new ChattyAIService(openAIAccount.key, Duration.ofSeconds(10),
-          "https://api.openai-proxy.com/");
+          baseUrl);
+
       LocalDate endDate = LocalDate.now().plusDays(1);
       Usage usage = chattyAIService.dashboardBillingUsage(endDate.plusDays(-99), endDate);
       System.out.println(openAIAccount.account + "\t\t: " + df.format(usage.getTotalUsage() / 100) + "$");
--- chattyai-proxy/main.go
+++ chattyai-proxy/main.go
@@ -2,54 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net"
 	"net/http"
-	"strings"
+	"net/http/httputil"
 )
 
-type Proxy struct{}
-
-func (p *Proxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	fmt.Printf("Received request %s %s %s\n", req.Method, req.Host, req.RemoteAddr)
-
-	transport := http.DefaultTransport
-
-	outReq := new(http.Request)
-	*outReq = *req
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		if prior, ok := outReq.Header["X-Forwarded-For"]; ok {
-			clientIP = strings.Join(prior, ", ") + ", " + clientIP
-		}
-		outReq.Header.Set("X-Forwarded-For", clientIP)
-	}
-
-	res, err := transport.RoundTrip(outReq)
-	if err != nil {
-		rw.WriteHeader(http.StatusBadGateway)
-		return
-	}
-
-	for key, value := range res.Header {
-		for _, v := range value {
-			rw.Header().Add(key, v)
-		}
-	}
-	rw.WriteHeader(res.StatusCode)
-	_, err = io.Copy(rw, res.Body)
-	if err != nil {
-		return
-	}
-	err = res.Body.Close()
-	if err != nil {
-		return
-	}
-}
-
 func main() {
-	fmt.Println("Serve on :8080")
-	http.Handle("/", &Proxy{})
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	fmt.Println("Serve on :38820")
+
+	director := func(req *http.Request) {
+		req.URL.Scheme = "https"
+		req.URL.Host = "api.openai.com"
+	}
+
+	http.Handle("/", &httputil.ReverseProxy{Director: director})
+
+	err := http.ListenAndServe(":38820", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
